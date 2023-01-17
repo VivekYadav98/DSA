38d163076377de89050b2cf8fd0e875d425e3c3e
Vivek yadav <20bec069@nith.ac.in>
2023-01-17 18:58:42 +0530

    Added solution - LeetHub

--- Insert Interval - GFG/insert-interval.cpp
+++ Insert Interval - GFG/insert-interval.cpp
@@ -20,10 +20,10 @@ class Solution {
                   flag = true;
                   i--;
               }
-              else if(flag == false && intervals[i][0] <= newEvent[0] && intervals[i][1] <= newEvent[0])
-              {
-                  ans.push_back(intervals[i]);
-              }
+            //   else if(flag == false && intervals[i][0] <= newEvent[0] && intervals[i][1] <= newEvent[0])
+            //   {
+            //       ans.push_back(intervals[i]);
+            //   }
               else if(flag == false && intervals[i][0] <= newEvent[0] && intervals[i][1] >= newEvent[1])
               {
                   flag = true;
