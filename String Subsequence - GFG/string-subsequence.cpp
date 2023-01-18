5d509152e84ed0264c0958eb3e67636e88d06124
Vivek yadav <20bec069@nith.ac.in>
2023-01-18 19:49:30 +0530

    Added solution - LeetHub

--- String Subsequence - GFG/string-subsequence.cpp
+++ String Subsequence - GFG/string-subsequence.cpp
@@ -9,33 +9,61 @@ using namespace std;
 
 class Solution{
 public:
-    int solve(string S1,string S2,int n,int m)
+    // int solve(string S1,string S2,int n,int m)
+    // {
+    //     if(m==0)
+    //     {
+    //       return 1;
+    //     }
+        
+    //     if(n == 0)
+    //     {
+    //         return 0;
+    //     }
+        
+    //     if(S1[n-1] == S2[m-1])
+    //     {
+    //         return solve(S1,S2,n-1,m-1) + solve(S1,S2,n-1,m);
+    //     }
+    //     else
+    //     {
+    //         return solve(S1,S2,n-1,m);
+    //     }
+    // }
+    
+    int solveMem(string S1,string S2,int n,int m,vector<vector<int>>& dp)
     {
-           if(m==0)
-           {
-               return 1;
-           }
+        if(m==0)
+        {
+           return 1;
+        }
         
         if(n == 0)
         {
             return 0;
         }
         
+        if(dp[n][m] != -1)
+        {
+            return dp[n][m];
+        }
+        
         if(S1[n-1] == S2[m-1])
         {
-            return solve(S1,S2,n-1,m-1) + solve(S1,S2,n-1,m);
+            return dp[n][m] = solveMem(S1,S2,n-1,m-1,dp) + solveMem(S1,S2,n-1,m,dp);
         }
         else
         {
-            return solve(S1,S2,n-1,m);
+            return dp[n][m] = solveMem(S1,S2,n-1,m,dp);
         }
     }
 
     int countWays(string S1, string S2){
         int n1 = S1.length();
         int n2 = S2.length();
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,-1));
 
-        return solve(S1,S2,n1,n2);
+        return solveMem(S1,S2,n1,n2,dp);
     }
 };
 
