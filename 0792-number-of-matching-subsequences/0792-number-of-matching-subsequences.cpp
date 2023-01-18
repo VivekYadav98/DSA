9c26f7d73dcff754132b4196a69b1ecf71b41132
Vivek yadav <20bec069@nith.ac.in>
2023-01-18 22:44:53 +0530

    Time: 274 ms (75.49%), Space: 51.4 MB (81.02%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
@@ -3,17 +3,30 @@ class Solution {
 public:
 
     int numMatchingSubseq(string s, vector<string>& words) {
-  int ans = 0;
+    int ans = 0;
         vector<vector<int>> mappings(26);  // stores array of indices for each character in s
-        for(int i = 0; i < size(s); i++) mappings[s[i] - 'a'].push_back(i);
-        for(auto& word : words) {
-            bool found = true;
+        for(int i = 0; i < size(s); i++)
+        {
+            mappings[s[i] - 'a'].push_back(i);
+        }
+        
+        for(auto& word : words) 
+        {
+            int  found = 1;
             // i = index in word | prev = index in s matched for word[i-1]
-            for(int i = 0, prev = -1; found && i < size(word); i++) {
+            for(int i = 0, prev = -1; found == 1 && i < word.size(); i++) 
+            {
                 auto& v = mappings[word[i]-'a'];
-                auto it = upper_bound(begin(v), end(v), prev);   // check if current character exists in s with index > prev
-                if(it == end(v)) found = false;                  // doesn't exist
-                else prev = *it;                                 // update prev for next check
+                auto it = upper_bound(v.begin(), v.end(), prev);  
+                // check if current character exists in s with index > prev
+                if(it == end(v))
+                {
+                    found = false;
+                }// doesn't exist
+                else
+                {
+                    prev = *it;   
+                }// update prev for next check
             }
             ans += found;
         }
