414ebab7b7fc4d4d29d3c9396f005d7bb7a7e036
Vivek yadav <20bec069@nith.ac.in>
2023-01-20 19:26:18 +0530

    Time: 14 ms (86.56%), Space: 7.1 MB (66.09%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string S = s;
+         int n = S.length();
+        int start = 0,end=1;
+        
+        for(int i=0;i<n;i++)
+        {
+            // EVEN LENGTH
+            int low = i-1;
+            int high = i;
+            
+            while(low>=0 && high<n && S[low] == S[high])
+            {
+                if(high-low+1 > end)
+                {
+                    end = high-low+1;
+                    start = low;
+                }
+                low--;
+                high++;
+            }
+            
+        //   ODD LENGTH
+            low = i-1;
+            high = i+1;
+            
+            while(low>=0 && high<n && S[low] == S[high])
+            {
+                if(high-low+1 > end)
+                {
+                    end = high-low+1;
+                    start = low;
+                }
+                low--;
+                high++;
+            }
+        }
+        
+        return S.substr(start,end);
+    }
+};
