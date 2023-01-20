2a96d3f3d3295f9e18f2465899f7662046edd2f3
Vivek yadav <20bec069@nith.ac.in>
2023-01-20 15:30:58 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -12,11 +12,7 @@ class Solution{
   public:
   int solve(string A,string B,int m,int n,vector<vector<int>>& dp)
   {
-      if(m == 0 && n == 0)
-      {
-          return 0;
-      }
-      else if(m<=0 || n<=0)
+      if(m == 0 || n == 0)
       {
           return 0;
       }
