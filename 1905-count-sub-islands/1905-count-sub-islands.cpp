b09e4115c773128af16e369b36f75f05919b5474
Vivek yadav <20bec069@nith.ac.in>
2023-01-20 22:03:50 +0530

    Time: 347 ms (74.08%), Space: 95.9 MB (38.29%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
      bool safe(int i,int j,int n,int m)
-   {
-      if(i<n && i>=0 && j<m && j>=0)
+  {
+      if(i<m && i>=0 && j<n && j>=0)
       {
           return true;
       }
@@ -10,63 +10,59 @@ public:
       {
           return false;
       }
-   }
+  }
     
     void dfs(vector<vector<int>>& grid2,vector<vector<bool>>& visited,
-             int i,int j)
+             int i,int j,int n,int m)
     {
-        int n = grid2.size();
-        int m = grid2[0].size();
-        
         visited[i][j] = true;
         
         if(safe(i-1,j,n,m) && !visited[i-1][j] && grid2[i-1][j] == 1)
         {
-          dfs(grid2,visited,i-1,j);
+          dfs(grid2,visited,i-1,j,n,m);
         }
            
           if(safe(i,j-1,n,m) && !visited[i][j-1] && grid2[i][j-1] == 1)
           {
-              dfs(grid2,visited,i,j-1);
+              dfs(grid2,visited,i,j-1,n,m);
           }
 
           if(safe(i,j+1,n,m) && !visited[i][j+1] && grid2[i][j+1] == 1)
           {
-              dfs(grid2,visited,i,j+1);
+              dfs(grid2,visited,i,j+1,n,m);
           }
 
           if(safe(i+1,j,n,m) && !visited[i+1][j] && grid2[i+1][j] == 1)
           {
-              dfs(grid2,visited,i+1,j);
+              dfs(grid2,visited,i+1,j,n,m);
           }
         
     } 
     
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        int n = grid1.size();
-        int m = grid1[0].size();
-        vector<vector<bool>>visited(n,vector<bool>(m,false));
+        int m = grid1.size(), n = grid1[0].size();
+        vector<vector<bool>>visited(m,vector<bool>(n,false));
         int count = 0;
         
-        for(int i=0;i<n;i++)
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
             {
-                if(grid2[i][j] == 1 && grid1[i][j] != 1)
+                if(!visited[i][j] && grid2[i][j] == 1 && grid1[i][j] != 1)
                 { 
-                    dfs(grid2,visited,i,j);
+                    dfs(grid2,visited,i,j,n,m);
                 }
             }
         }
         
-        for(int i=0;i<n;i++)
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=0;j<n;j++)
             {
                 if(!visited[i][j] && grid2[i][j] == 1)
                 { 
-                    dfs(grid2,visited,i,j);
-                     count++;
+                    count++;
+                    dfs(grid2,visited,i,j,n,m);
                 }
             }
         }
