e846ae63aac570458b33ac3bf6e780ffd52c642c
Vivek yadav <20bec069@nith.ac.in>
2023-01-22 10:50:28 +0530

    Time: 140 ms (68.38%), Space: 57.6 MB (46.13%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isPalindrome(string s,int start,int end)
+    {
+        while(start<=end)
+        {
+            if(s[start] != s[end])
+            {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    void solve(string s,int index,vector<vector<string>>& ans,vector<string>& path)
+    {
+        if(index == s.size())
+        {
+            ans.push_back(path);
+            return;
+        }
+        
+        for(int i=index;i<s.length();i++)
+        {
+            if(isPalindrome(s,index,i))
+            {
+                path.push_back(s.substr(index,i-index+1));
+                solve(s,i+1,ans,path);
+                path.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>path;
+        solve(s,0,ans,path);
+        return ans;
+    }
+};
