4fe659504c6203a82e94444f0c7f535338689539
Vivek yadav <20bec069@nith.ac.in>
2023-01-25 00:58:44 +0530

    Added solution - LeetHub

---
+++ Type it! - GFG/type-it.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int minOperation(string s) {
+        if(s.length() == 1)
+        {
+            return 1;
+        }
+        
+       int ans = 0;
+       int j=1;
+       
+       while(j<s.length())
+       {
+           int count = 0;
+           int k = j;
+           for(int i=0;i<j;i++)
+               {
+                   if(s[k] == s[i])
+                   {
+                       count++;
+                       k++;
+                       ans = max(ans,count);
+                   }
+                   else
+                   {
+                       i=j;
+                   }
+               }
+               j++;
+       }
+       
+       int n = s.length();
+       return n-ans+1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.minOperation(s) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
