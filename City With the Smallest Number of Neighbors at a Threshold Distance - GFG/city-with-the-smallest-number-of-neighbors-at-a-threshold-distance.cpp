99a4710b4e1a057dea4c218f6a94e1544fe3b3a6
Vivek yadav <20bec069@nith.ac.in>
2023-01-31 21:28:05 +0530

    Added solution - LeetHub

--- City With the Smallest Number of Neighbors at a Threshold Distance - GFG/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ City With the Smallest Number of Neighbors at a Threshold Distance - GFG/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -9,11 +9,11 @@ using namespace std;
 class Solution {
   public:
     int findCity(int n, int m, vector<vector<int>>& edges,int distanceThreshold) {
-         vector<vector<int>>cost(n,vector<int>(n,INT_MAX));
+         vector<vector<int>>distance(n,vector<int>(n,INT_MAX));
          for(auto it:edges)
          {
-             cost[it[0]][it[1]] = it[2];
-             cost[it[1]][it[0]] = it[2];
+             distance[it[0]][it[1]] = it[2];
+             distance[it[1]][it[0]] = it[2];
          }
          
          for(int k=0;k<n;k++)
@@ -22,18 +22,18 @@ class Solution {
              {
                  for(int j=0;j<n;j++)
                  {
-                     if(cost[i][k] == INT_MAX || cost[k][j] == INT_MAX)
+                     if(distance[i][k] == INT_MAX || distance[k][j] == INT_MAX)
                      {
                          continue;
                      }
-                     cost[i][j] = min(cost[i][j],cost[i][k]+cost[k][j]);
+                     distance[i][j] = min(distance[i][j],distance[i][k]+distance[k][j]);
                  }
              }
          }
          
          for(int i=0;i<n;i++)
          {
-             cost[i][i] = 0;
+             distance[i][i] = 0;
          }
          
          int count = n;
@@ -44,7 +44,7 @@ class Solution {
              int cnt = 0;
              for(int j=0;j<n;j++)
              {
-                 if(cost[i][j] <= distanceThreshold)
+                 if(distance[i][j] <= distanceThreshold)
                  {
                      cnt++;
                  }
