7e7e73115003d4b0ee525aed2efa776c97dd03ec
Vivek yadav <20bec069@nith.ac.in>
2023-02-04 16:17:29 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.cpp
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -38,35 +38,17 @@ public:
     {
         vector<vector<bool>>visited(n,vector<bool>(m,false));
         
-        for(int i=0;i<m;i++)
-        {
-            if(!visited[0][i] && mat[0][i] == 'O')
-            {
-                dfs(0,i,n,m,mat,visited);
-            }
-        }
-        
         for(int i=0;i<n;i++)
         {
-            if(!visited[i][0] && mat[i][0] == 'O')
+            for(int j=0;j<m;j++)
             {
-                dfs(i,0,n,m,mat,visited);
-            }
-        }
-        
-        for(int i=0;i<m;i++)
-        {
-            if(!visited[n-1][i] && mat[n-1][i] == 'O')
-            {
-                dfs(n-1,i,n,m,mat,visited);
-            }
-        }
-        
-        for(int i=0;i<n;i++)
-        {
-            if(!visited[i][m-1] && mat[i][m-1] == 'O')
-            {
-                dfs(i,m-1,n,m,mat,visited);
+                if(i==0 || j==0 || i==n-1 || j==m-1)
+                {
+                    if(!visited[i][j] && mat[i][j] == 'O')
+                    {
+                        dfs(i,j,n,m,mat,visited);
+                    }
+                 }
             }
         }
         
