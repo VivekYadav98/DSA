3f9923aca1b6657c1e28fa24f417c752d5b0b684
Vivek yadav <20bec069@nith.ac.in>
2023-02-14 01:21:55 +0530

    Added solution - LeetHub

--- Shortest Common Supersequence - GFG/shortest-common-supersequence.cpp
+++ Shortest Common Supersequence - GFG/shortest-common-supersequence.cpp
@@ -40,39 +40,6 @@ class Solution
             }
         }
         
-        int i=m,j=n;
-        string ans = "";
-        while(i>0 && j>0)
-        {
-            if(X[i-1] == Y[j-1])
-            {
-                ans += X[i-1];
-                i--;
-                j--;
-            }
-            else if(dp[i-1][j] > dp[i][j-1])
-            {
-                ans += X[i-1];
-                i--;
-            }
-            else
-            {
-                ans += Y[j-1];
-                j--;
-            }
-        }
-        
-        while(i>0)
-        {
-            ans += X[i-1];
-            i--;
-        }
-        while(j>0)
-        {
-            ans += Y[j-1];
-            j--;
-        }
-        reverse(ans.begin(),ans.end());
         return m+n-dp[m][n];
     }
 };
