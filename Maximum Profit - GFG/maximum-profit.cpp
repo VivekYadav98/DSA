54b50ea8c3816ef3621628fb66804275a8e70cd4
Vivek yadav <20bec069@nith.ac.in>
2023-02-15 19:27:23 +0530

    Added solution - LeetHub

--- Maximum Profit - GFG/maximum-profit.cpp
+++ Maximum Profit - GFG/maximum-profit.cpp
@@ -9,28 +9,9 @@ using namespace std;
 
 class Solution {
   public:
-    int solve(int ind,int buy,int cap,int prices[],vector<vector<vector<int>>>& dp,int N)
-    {
-        if(cap == 0) return 0;
-        if(ind == N) return 0;
-
-        if(dp[ind][buy][cap] != -1)
-        {
-            return dp[ind][buy][cap];
-        }
-        int profit = 0;
-        if(buy)
-        {
-            profit = max(-prices[ind] + solve(ind+1,0,cap,prices,dp,N) , 0 + solve(ind+1,1,cap,prices,dp,N));
-        }
-        else
-        {
-            profit = max(prices[ind] + solve(ind+1,1,cap-1,prices,dp,N) , 0 + solve(ind+1,0,cap,prices,dp,N));
-        }
-        return dp[ind][buy][cap] = profit;
-    }
     int maxProfit(int K, int N, int A[]) {
-        vector<vector<vector<int>>>dp(N+1,vector<vector<int>>(2,vector<int>(K+1,0)));
+        vector<vector<int>>next(2,vector<int>(K+1,0));
+        vector<vector<int>>curr(2,vector<int>(K+1,0));
 
         for(int ind=N-1;ind>=0;ind--)
         {
@@ -38,21 +19,22 @@ class Solution {
             {
                 for(int cap=1;cap<=K;cap++)
                 {
-                    long profit = 0;
+                    int profit = 0;
                     if(buy)
                     {
-                        profit = max(-A[ind] + dp[ind+1][0][cap] , 0 + dp[ind+1][1][cap]);
+                        profit = max(-A[ind] + next[0][cap] , 0 + next[1][cap]);
                     }
                     else
                     {
-                        profit = max(A[ind] + dp[ind+1][1][cap-1] , 0 + dp[ind+1][0][cap]);
+                        profit = max(A[ind] + next[1][cap-1] , 0 + next[0][cap]);
                     }
-                    dp[ind][buy][cap] = profit;
+                    curr[buy][cap] = profit;
                 }
+                next = curr;
             }
         }
 
-        return dp[0][1][K];
+        return next[1][K];
     }
 };
 
