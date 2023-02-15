54bd090c0b3ad484c78af2b5ca7c20c32a89789b
Vivek yadav <20bec069@nith.ac.in>
2023-02-15 12:13:19 +0530

    Added solution - LeetHub

---
+++ Maximum Difference - GFG/maximum-difference.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    void solve(int A[],int n,vector<int>& ls)
+    {
+          stack<int>s;
+          stack<int>temp;
+          ls.push_back(0);
+          s.push(A[0]);
+          int i=1;
+          
+          while(i<n)
+          {
+             if(!s.empty() && s.top() < A[i])
+             {
+                 ls.push_back(s.top());
+                 s.push(A[i]);
+                 i++;
+                 
+                //  while(!temp.empty())
+                //  {
+                //      s.push(temp.top());
+                //      temp.pop();
+                //  }
+             }
+             else if(!s.empty())
+             {
+                temp.push(s.top());
+                s.pop(); 
+             }
+             else
+             {
+                 ls.push_back(0);
+                 s.push(A[i]);
+                 i++;
+             }
+          }
+    }
+    /*You are required to complete this method */
+    int findMaxDiff(int A[], int n)
+    {
+      vector<int>ls;
+      vector<int>rs;
+      
+      solve(A,n,ls);
+      
+      reverse(A,A+n);
+      solve(A,n,rs);
+      
+     reverse(rs.begin(),rs.end());
+      
+      int ans = INT_MIN;
+      for(int i=0;i<n;i++)
+      {
+          ans = max(ans , abs(ls[i]-rs[i]));
+      }
+      return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+   	int n;
+   	cin>>n;
+   	int a[n];
+   	for(int i=0;i<n;i++)
+   	cin>>a[i];
+   	Solution ob;
+   	cout<<ob.findMaxDiff(a,n)<<endl;
+   }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
