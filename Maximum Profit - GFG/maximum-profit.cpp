14638bbc7c0ee3476ed6500e9f10dc6b44ab7ee2
Vivek yadav <20bec069@nith.ac.in>
2023-02-15 19:26:31 +0530

    Added solution - LeetHub

--- Maximum Profit - GFG/maximum-profit.cpp
+++ Maximum Profit - GFG/maximum-profit.cpp
@@ -30,8 +30,29 @@ class Solution {
         return dp[ind][buy][cap] = profit;
     }
     int maxProfit(int K, int N, int A[]) {
-        vector<vector<vector<int>>>dp(N+1,vector<vector<int>>(2,vector<int>(K+1,-1)));
-        return solve(0,1,K,A,dp,N);
+        vector<vector<vector<int>>>dp(N+1,vector<vector<int>>(2,vector<int>(K+1,0)));
+
+        for(int ind=N-1;ind>=0;ind--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                for(int cap=1;cap<=K;cap++)
+                {
+                    long profit = 0;
+                    if(buy)
+                    {
+                        profit = max(-A[ind] + dp[ind+1][0][cap] , 0 + dp[ind+1][1][cap]);
+                    }
+                    else
+                    {
+                        profit = max(A[ind] + dp[ind+1][1][cap-1] , 0 + dp[ind+1][0][cap]);
+                    }
+                    dp[ind][buy][cap] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][K];
     }
 };
 
