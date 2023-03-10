5363a81caa1a38d4b89a3e6bdf8456ad123c04b3
Vivek yadav <20bec069@nith.ac.in>
2023-03-11 00:11:53 +0530

    Added solution - LeetHub

--- Jump Game - GFG/jump-game.cpp
+++ Jump Game - GFG/jump-game.cpp
@@ -16,7 +16,11 @@ class Solution {
             return true;
         }
 
-        
+        if(dp[i] != -1)
+        {
+            return dp[i];
+        }
+
         for(int jump=1;jump<=nums[i];jump++)
         {
             if(solve(i+jump , nums,dp,n))
@@ -24,7 +28,7 @@ class Solution {
                 return true;
             }
         }
-        return false;
+        return dp[i] = false;
     }
     int canReach(int A[], int N) {
         vector<int>dp(N+1,-1);
