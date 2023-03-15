a92e65a42574349094d1b160ce8619d065bfc7ef
Vivek yadav <20bec069@nith.ac.in>
2023-03-15 15:27:36 +0530

    Added solution - LeetHub

--- Special Palindrome Substrings - GFG/special-palindrome-substrings.cpp
+++ Special Palindrome Substrings - GFG/special-palindrome-substrings.cpp
@@ -11,48 +11,39 @@ class Solution{
     //Complete the function and return minimum number of operations
     public:
     int specialPalindrome(string s1, string s2){
-        bool flag = false;
-        if(s1.find(s2) != string::npos)
-        {
-            flag = true;
-        }
-        
         int ans = INT_MAX;
-        if(flag == false)
+        for(int i=0;i<=s1.length()-s2.length();i++)
         {
-            for(int i=0;i<=s1.length()-s2.length();i++)
+            int cnt = 0;
+            string temp = s1;
+            int k=i;
+            for(int j=0;j<s2.length();j++)
             {
-                int cnt = 0;
-                string temp = s1;
-                int k=i;
-                for(int j=0;j<s2.length();j++)
+                if(temp[k] != s2[j])
                 {
-                    if(temp[k] != s2[j])
-                    {
-                        cnt++;
-                    }
-                    temp[k] = s2[j];
-                    k++;
+                    cnt++;
                 }
-                
-                int s=0,e=temp.length()-1;
-                string temp1 = temp;
-                while(s<e)
+                temp[k] = s2[j];
+                k++;
+            }
+            
+            int s=0,e=temp.length()-1;
+            string temp1 = temp;
+            while(s<e)
+            {
+                if(temp[s] != temp[e])
                 {
-                    if(temp[s] != temp[e])
-                    {
-                        temp[e] = temp[s];
-                        temp1[s] = temp1[e];
-                        cnt++;
-                    }
-                    s++;
-                    e--;
+                    temp[e] = temp[s];
+                    temp1[s] = temp1[e];
+                    cnt++;
                 }
-                
-                if(((temp.find(s2) != string::npos) || (temp1.find(s2) != string::npos)) && cnt<ans)
-                {
-                    ans = min(ans,cnt);
-                }
+                s++;
+                e--;
+            }
+            
+            if(((temp.find(s2) != string::npos) || (temp1.find(s2) != string::npos)) && cnt<ans)
+            {
+                ans = min(ans,cnt);
             }
         }
         
