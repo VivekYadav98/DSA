a087b58a05c2892e112a077522b195fe3ba6dae5
Vivek yadav <20bec069@nith.ac.in>
2023-03-26 00:41:55 +0530

    Added solution - LeetHub

---
+++ Nearest multiple of 10 - GFG/nearest-multiple-of-10.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    string roundToNearest(string N) 
+    { 
+        int n = N.length();
+        int r = N[n-1] - '0';
+        if(r<=5)
+        {
+            N[n-1] = 0 +'0';
+            return N;
+        }
+        else
+        {
+            N[n-1] = 0 +'0';
+            int carry = 1;
+            for(int i=n-2;i>=0;i--)
+            {
+                int num = N[i]-'0';
+                num = num+carry;
+                int digit = num%10;
+                carry = num/10;
+                N[i] = digit+'0';
+            }
+            
+            if(carry != 0)
+            {
+                N.insert(N.begin(),'1');
+            }
+            return N;
+        }
+    }  
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    
+    int t;cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout << ob.roundToNearest(s) << endl;
+    }
+    
+	return 0;
+}
+// } Driver Code Ends
