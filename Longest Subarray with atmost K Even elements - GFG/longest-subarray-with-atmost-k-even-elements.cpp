47a0c8c5a7df881d3ea1b07fa5997fbfda04f96b
Vivek yadav <20bec069@nith.ac.in>
2023-04-07 20:02:04 +0530

    Added solution - LeetHub

---
+++ Longest Subarray with atmost K Even elements - GFG/longest-subarray-with-atmost-k-even-elements.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n,k;
+	    cin >> n >> k;
+	    
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin >> arr[i];
+	    }
+	    
+	    int cnt = 0;
+	    int ans = 0;
+	    int i=0,j=0;
+	    
+	    while(j<n)
+	    {
+	        if(arr[j]%2 == 0)
+	        {
+	            cnt++;
+	        }
+	        
+	        if(cnt <= k)
+	        {
+	            ans = max(ans,j-i+1);
+	            j++;
+	        }
+	        else if(cnt > k)
+	        {
+	            while(cnt > k && i<j)
+	            {
+	                if(arr[i]%2 == 0)
+	                {
+	                    cnt--;
+	                }
+	                i++;
+	            }
+	            j++;
+	        }
+	    }
+	    
+	    if(ans == 0)
+	    {
+	      ans = n;
+	    }
+	    
+	    cout << ans << endl;
+	}
+	return 0;
+}
