eb134f1d86b55105a518ea138638d2b4e8d2b6a2
Vivek yadav <20bec069@nith.ac.in>
2023-04-14 00:49:06 +0530

    Added solution - LeetHub

---
+++ Pascal Triangle - GFG/pascal-triangle.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+// #define ll long long
+class Solution{
+public:
+    int mod = 1000000007;
+    vector<ll> nthRowOfPascalTriangle(int n) {
+        vector<vector<ll>>ans;
+        if(n == 1)
+        {
+            return {1};
+        }
+
+        vector<ll>temp;
+        ans.push_back({1});
+
+        int cnt = 2;
+        while(cnt <= n)
+        {
+            temp = ans.back();
+
+            vector<ll>arr;
+            arr.push_back(1);
+            for(int i=1;i<temp.size();i++)
+            {
+                long long int num = (temp[i] + temp[i-1])%mod;
+                arr.push_back(num);
+            }
+            
+            arr.push_back(1);
+            ans.push_back(arr);
+            cnt++;
+        }
+
+        return ans[n-1];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+void printAns(vector<ll> &ans) {
+    for (auto &x : ans) {
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.nthRowOfPascalTriangle(n);
+        printAns(ans);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
