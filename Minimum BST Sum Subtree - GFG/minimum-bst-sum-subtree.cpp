f8134fb94df713953dd0009a228ae0a61798e9ee
Vivek yadav <20bec069@nith.ac.in>
2023-05-17 22:51:29 +0530

    Added solution - LeetHub

---
+++ Minimum BST Sum Subtree - GFG/minimum-bst-sum-subtree.cpp
@@ -0,0 +1,163 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+
+    Node *buildTree(string s) {
+        if (s.length() == 0 || s[0] == 'N') return NULL;
+
+        // Create the root of the tree
+        vector<string> str;
+        istringstream iss(s);
+        for (string s; iss >> s;) str.push_back(s);
+        Node *root = new Node(stoi(str[0]));
+
+        // Push the root to the queue
+        queue<Node*> q;
+        q.push(root);
+
+        // Starting from the second element
+        int i = 1;
+        while (q.size() > 0 && i < str.size()) {
+
+            // Get and remove the front of the queue
+            Node *currNode = q.front();
+            q.pop();
+
+            // Get the current node's value from the string
+            string currVal = str[i];
+
+            // If the left child is not null
+            if (currVal != "N") {
+
+                // Create the left child for the current node
+                currNode->left = new Node(stoi(currVal));
+                // Push it to the queue
+                q.push(currNode->left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= str.size()) break;
+
+            currVal = str[i];
+
+            // If the right child is not null
+            if (currVal != "N") {
+
+                // Create the right child for the current node
+                currNode->right = new Node(stoi(currVal));
+
+                // Push it to the queue
+                q.push(currNode->right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    void inorder(Node *root) {
+        if (root == NULL) return;
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    int ans = INT_MAX;
+    bool isBST(Node* root,int mini,int maxi)
+    {
+        if(root == NULL)
+        {
+            return true;
+        }
+        
+        if(root->data > mini && root->data < maxi)
+        {
+            return isBST(root->left,mini,root->data) && isBST(root->right,root->data,maxi);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    int findSum(Node* root,int& cnt)
+    {
+        if(root == NULL)
+        {
+            return 0;
+        }
+        
+        cnt++;
+        return root->data + findSum(root->left,cnt) + findSum(root->right,cnt);
+    }
+    
+    int solve(int target,Node* root)
+    {
+        if(root == NULL)
+        {
+            return ans;
+        }
+        
+        int cnt = 0;
+        if(isBST(root,INT_MIN,INT_MAX) && findSum(root,cnt) == target)
+        {
+            ans = min(ans,cnt);
+        }
+        
+        return min(solve(target,root->left),solve(target,root->right));
+    }
+    
+    int minSubtreeSumBST(int target, Node *root) {
+        int res = solve(target,root);
+         if(res == INT_MAX)
+         {
+             return -1;
+         }
+         return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int target;
+        cin>>target;
+        string str;
+        getline(cin,str);
+        getline(cin,str);
+        Node node(0);
+        Node *root = node.buildTree(str);
+        Solution ob;
+        int res = ob.minSubtreeSumBST(target, root);
+        cout<<res<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
