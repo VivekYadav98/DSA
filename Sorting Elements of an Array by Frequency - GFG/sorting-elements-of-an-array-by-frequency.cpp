0222ef2660a0b9f54cb00fbb895c4ac4e659dc08
Vivek yadav <20bec069@nith.ac.in>
2023-06-07 22:19:05 +0530

    Added solution - LeetHub

--- Sorting Elements of an Array by Frequency - GFG/sorting-elements-of-an-array-by-frequency.cpp
+++ Sorting Elements of an Array by Frequency - GFG/sorting-elements-of-an-array-by-frequency.cpp
@@ -1,31 +1,24 @@
-#include <iostream>
-#include<bits/stdc++.h>
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
 
- bool cmp(pair<int,int>& a,pair<int,int>& b)
-{
-    if(a.first == b.first)
-    {
-        return a.second<b.second;
-    }
-    return a.first>b.first;
-}
 
-int main() {
-	int t;
-	cin>>t;
-	
-	while(t--)
-	{
-	    int n;
-	    cin>>n;
-	    int arr[n];
-	    for(int i=0;i<n;i++)
-	    {
-	        cin >> arr[i];
-	    }
-	    
-	    unordered_map<int,int>m;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    static bool cmp(pair<int,int>& a,pair<int,int>& b)
+    {
+        if(a.first == b.first)
+        {
+            return a.second<b.second;
+        }
+        return a.first>b.first;
+    }
+    
+    vector<int> sortByFreq(int arr[],int n)
+    {
+        unordered_map<int,int>m;
         vector<int>ans;
         for(int i=0;i<n;i++)
         {
@@ -52,13 +45,40 @@ int main() {
             }
             i++;
         }
-        
-        for(auto i:ans)
-        {
-            cout<<i<<" ";
-        }
-        cout<<endl;
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	
+	int t;
+	cin >> t;
+	
+	
+	while(t--){
+	    
+	    
+	    int n;
+	    cin >> n;
+	    
+	    int a[n+1];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	    }
+	    Solution obj;
+	    vector<int> v;
+	    v = obj.sortByFreq(a,n);
+	    for(int i:v)
+	        cout<<i<<" ";
+	    cout << endl;
 	}
 	
 	return 0;
 }
+
+
+// } Driver Code Ends
