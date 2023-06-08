6ba972968411ad351c9a4a6844b04f47a5c768aa
Vivek yadav <20bec069@nith.ac.in>
2023-06-08 23:20:59 +0530

    Added solution - LeetHub

---
+++ Intersection of Two Linked Lists - GFG/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+
+// } Driver Code Ends
+/* structure of list node:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution{
+  public:
+    Node* findIntersection(Node* head1, Node* head2)
+    {
+        Node* curr1 = head1,*curr2=head2;
+        unordered_map<int,int>m;
+        while(curr2 != NULL)
+        {
+            m[curr2->data]++;
+            curr2 = curr2->next;
+        }
+        
+        Node* head = new Node(-1);
+        Node* tail = head;
+        while(curr1 != NULL)
+        {
+            if(m.find(curr1->data) != m.end())
+            {
+                tail->next = new Node(curr1->data);
+                tail = tail->next;
+            }
+            curr1 = curr1->next;
+        }
+        
+        return head->next;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    
+	    cin>> n;
+	    Node* head1 = inputList(n);
+	    
+	    cin>>m;
+	    Node* head2 = inputList(m);
+	    Solution obj;
+	    Node* result = obj.findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
