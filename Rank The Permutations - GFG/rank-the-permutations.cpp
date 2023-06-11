11975d5dacb3b61013b1505f038e96162aa24d1d
Vivek yadav <20bec069@nith.ac.in>
2023-06-11 21:12:55 +0530

    Added solution - LeetHub

---
+++ Rank The Permutations - GFG/rank-the-permutations.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    long long fact(int n)
+    {
+        if(n<=1)
+        {
+            return 1;
+        }
+        
+        return n*fact(n-1);
+    }
+    long long findRank(string str) {
+        long long ans = 1;
+        vector<int>cnt(26,0);
+        
+        int n = str.length();
+        long long mul = fact(n);
+        for(int i=0;i<str.length();i++)
+        {
+            cnt[str[i]-'a']++;
+        }
+        
+        for(int i=1;i<26;i++)
+        {
+            cnt[i] += cnt[i-1];
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            mul = mul/(n-i);
+            
+            ans += cnt[str[i]-'a' -1]*mul;
+            
+            for(int j=str[i]-'a';j<26;j++)
+            {
+                cnt[j]--;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        string s;
+        cin>>s;
+        Solution obj;
+        long long ans = obj.findRank(s);
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
