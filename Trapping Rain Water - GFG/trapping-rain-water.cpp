1616071bc3f7b256ad85d1593f1ac1f79206ce5f
Vivek yadav <20bec069@nith.ac.in>
2023-06-14 02:02:25 +0530

    Added solution - LeetHub

--- Trapping Rain Water - GFG/trapping-rain-water.cpp
+++ Trapping Rain Water - GFG/trapping-rain-water.cpp
@@ -10,26 +10,27 @@ class Solution{
     // Function to find the trapped water between the blocks.
     public:
     long long trappingWater(int arr[], int n){
-       long long ans = 0;
-       int left[n];
-       int right[n];
-       left[0] = arr[0];
-       for(int i=1;i<n;i++)
-       {
-           left[i] = max(left[i-1] , arr[i]);
-       }
-       
-       right[n-1] = arr[n-1];
-       for(int i=n-2;i>=0;i--)
-       {
-           right[i] = max(right[i+1] , arr[i]);
-       }
-       
-       for(int i=0;i<n;i++)
-       {
-           ans += min(left[i],right[i]) - arr[i];
-       }
-       return ans;
+        vector<int>left(n,0);
+        vector<int>right(n,0);
+        left[0] = arr[0];
+        
+        for(int i=1;i<n;i++)
+        {
+            left[i] = max(arr[i],left[i-1]);
+        }
+        
+        right[n-1] = arr[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i] = max(arr[i],right[i+1]);
+        }
+        
+        long long ans =0;
+        for(int i=1;i<n-1;i++)
+        {
+            ans += min(left[i],right[i])-arr[i];
+        }
+        return ans;
     }
 };
 
