882ebb4cd3aa686c7f589c3718a763b57cb14020
Vivek yadav <20bec069@nith.ac.in>
2023-06-23 00:51:39 +0530

    Added solution - LeetHub

--- Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
+++ Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
@@ -14,6 +14,27 @@ for (int i=0; i < size; i++)
 // } Driver Code Ends
 //User function Template for C++
 
+class Node{
+    public:
+    int data;
+    int row;
+    int col;
+    
+    Node(int data,int row,int col)
+    {
+        this->data = data;
+        this->row = row;
+        this->col = col;
+    }
+};
+
+class cmp{
+    public:
+    bool operator()(Node* a,Node* b)
+    {
+        return (a->data > b->data);
+    }
+};
 
 class Solution
 {
@@ -21,20 +42,30 @@ class Solution
     //Function to merge k sorted arrays.
     vector<int> mergeKArrays(vector<vector<int>> arr, int K)
     {
-        priority_queue<int,vector<int>,greater<int>>pq;
-        for(int i=0;i<arr.size();i++)
+        priority_queue<Node*,vector<Node*>,cmp>pq;
+        
+        for(int i=0;i<K;i++)
         {
-            for(int j=0;j<arr[i].size();j++)
-            {
-                pq.push(arr[i][j]);
-            }
+            pq.push(new Node(arr[i][0],i,0));
         }
         
         vector<int>ans;
+        
         while(!pq.empty())
         {
-            ans.push_back(pq.top());
+            Node* temp = pq.top();
             pq.pop();
+            
+            int val = temp->data;
+            int row = temp->row;
+            int col = temp->col;
+            
+            ans.push_back(val);
+            
+            if(col+1 < arr[row].size())
+            {
+                pq.push(new Node(arr[row][col+1],row,col+1));
+            }
         }
         return ans;
     }
