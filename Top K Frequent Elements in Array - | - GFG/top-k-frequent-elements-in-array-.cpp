09fd173cb35ccca55b929b13406ce3d022b87e16
Vivek yadav <20bec069@nith.ac.in>
2023-06-22 23:08:11 +0530

    Added solution - LeetHub

--- Top K Frequent Elements in Array - | - GFG/top-k-frequent-elements-in-array-.cpp
+++ Top K Frequent Elements in Array - | - GFG/top-k-frequent-elements-in-array-.cpp
@@ -5,39 +5,27 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  static bool cmp(pair<int,int>a,pair<int,int>b)
-    {
-        if(a.second == b.second)
-        {
-            return a.first>b.first;
-        }
-        else
-        {
-            return (a.second>b.second);
-        }
-    }
-    
     vector<int> topK(vector<int>& nums, int k) {
-         map<int,int>m;
-        for(auto i:nums)
-        {
-            m[i]++;
-        }
-        
-        vector<pair<int,int>>ans;
-        for(auto i:m)
-        {
-           ans.push_back({i.first,i.second});
-        }
-        
-        sort(ans.begin(),ans.end(),cmp);
-        
-        vector<int>res;
-        for(int i=0;i<k;i++)
-        {
-            res.push_back(ans[i].first);
-        }
-        return res;
+       unordered_map<int,int>m;
+       for(auto i:nums)
+       {
+           m[i]++;
+       }
+       
+       priority_queue<pair<int,int>>pq;
+       
+       for(auto i:m)
+       {
+           pq.push({i.second,i.first});
+       }
+       
+       vector<int>ans;
+       while(k--)
+       {
+           ans.push_back(pq.top().second);
+           pq.pop();
+       }
+       return ans;
     }
 };
 
