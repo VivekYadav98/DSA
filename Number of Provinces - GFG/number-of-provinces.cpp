7d5cd6e6899ebff3d9e3bf43cec1c821f18381d4
Vivek yadav <20bec069@nith.ac.in>
2023-06-25 18:34:01 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.cpp
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -6,78 +6,47 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-class DisjointSet{
-    public:
-    vector<int>parent,rank;
-       DisjointSet(int n)
-       {
-           rank.resize(n+1,0);
-           parent.resize(n+1);
-           for(int i=0;i<=n;i++)
-           {
-               parent[i] = i;
-           }
-       }
-       
-       int findUpar(int node)
-       {
-           if(node == parent[node])
-           {
-               return node;
-           }
-           
-           return findUpar(parent[node]);
-       }
-       
-       void unionByRank(int u,int v)
-       {
-           int ulpU = findUpar(u);
-           int ulpV = findUpar(v);
-           
-           if(ulpU == ulpV)
-           {
-               return;
-           }
-           
-           if(rank[ulpU] < rank[ulpV])
-           {
-               parent[ulpU] = ulpV;
-           }
-           else if(rank[ulpV] < rank[ulpU])
-           {
-               parent[ulpV] = ulpU;
-           }
-           else
-           {
-               parent[ulpV] = ulpU;
-               rank[ulpU]++;
-           }
-       }
-};
 class Solution {
   public:
+    void dfs(int node,vector<vector<int>>& adj,vector<bool>& vis)
+    {
+        vis[node] = true;
+        
+        for(auto it:adj[node])
+        {
+            if(!vis[it])
+            {
+                dfs(it,adj,vis);
+            }
+        }
+    }
+  
     int numProvinces(vector<vector<int>> adj, int V) {
-       DisjointSet ds(V);
-       for(int i=0;i<V;i++)
-       {
-           for(int j=0;j<V;j++)
-           {
-               if(adj[i][j] == 1)
-               {
-                   ds.unionByRank(i,j);
-               }
-           }
-       }
-       
-       int count = 0;
-       for(int i=0;i<V;i++)
-       {
-           if(ds.parent[i] == i)
-           {
-               count++;
-           }
-       }
-       return count;
+        vector<vector<int>>newAdj(V);
+        for(int i=0;i<V;i++)
+        {
+            for(int j=0;j<V;j++)
+            {
+                if(adj[i][j] == 1)
+                {
+                    newAdj[i].push_back(j);
+                    newAdj[j].push_back(i);
+                }
+            }
+        }
+        
+        vector<bool>vis(V,false);
+        int ans = 0;
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            {
+                ans++;
+                dfs(i,newAdj,vis);
+            }
+        }
+        
+        return ans;
     }
 };
 
