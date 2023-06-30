ab29d80e92d546f8876d6e5378143795592aaf19
Vivek yadav <20bec069@nith.ac.in>
2023-06-30 21:19:28 +0530

    Added solution - LeetHub

---
+++ Check if there exists a subsequence with sum K - GFG/check-if-there-exists-a-subsequence-with-sum-k.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    bool solve(int i,vector<int>&A,int target,vector<vector<int>>& dp)
+    {
+       if(target == 0)
+       {
+           return true;
+       }
+       
+       if(i>=A.size() || target<0)
+       {
+           return false;
+       }
+       
+       if(dp[i][target] != -1)
+       {
+           return dp[i][target];
+       }
+       
+       bool notTake = solve(i+1,A,target,dp);
+       bool take = false;
+       
+       if(target >= A[i])
+       {
+           take = solve(i+1,A,target-A[i],dp);
+       }
+       
+       return dp[i][target] = (take || notTake);
+    }
+    
+    bool checkSubsequenceSum(int n, vector<int>& arr, int k) {
+      vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
+       return solve(0,arr,k,dp);   
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k; cin>>n>>k;
+        vector<int> arr;
+        
+        for(int i=0; i<n; ++i){
+            int x; cin>>x;
+            arr.push_back(x);
+        }
+        
+        Solution obj;
+        bool ans = obj.checkSubsequenceSum(n, arr, k);
+        cout<<( ans ? "Yes" : "No")<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
