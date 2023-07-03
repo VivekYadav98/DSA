3ea27bf71a44a59ea7ab421092dec92cd7dfa3b2
Vivek yadav <20bec069@nith.ac.in>
2023-07-04 04:03:51 +0530

    Added solution - LeetHub

--- Search insert position of K in a sorted array - GFG/search-insert-position-of-k-in-a-sorted-array.cpp
+++ Search insert position of K in a sorted array - GFG/search-insert-position-of-k-in-a-sorted-array.cpp
@@ -7,30 +7,35 @@ using namespace std;
 
 class Solution{
     public:
-    int searchInsertK(vector<int>arr, int n, int x)
+    int searchInsertK(vector<int>Arr, int N, int k)
     {
-        if(x<arr[0])
-        {
-            return 0;
-        }
-
-        int s=0,e=n-1;
-        int i=0,j=0;
-        int mid = 0;
+        int s=0,e=N-1,mid;
         
         while(s<=e)
         {
-            mid = e + (s-e)/2;
+            mid = s+(e-s)/2;
             
-            if(arr[mid] == x)
+            if(Arr[mid] == k)
             {
                 return mid;
             }
-            else if(mid+1<n && arr[mid]<x && arr[mid+1]>x)
+            else if(mid == 0 && k<=Arr[mid])
+            {
+                return 0;
+            }
+            else if(mid == N-1 && k>=Arr[mid])
+            {
+                return N;
+            }
+            else if(k > Arr[mid-1] && k< Arr[mid])
+            {
+                return mid;
+            }
+            else if(k>Arr[mid] && k<Arr[mid+1])
             {
                 return mid+1;
             }
-            else if(arr[mid]<x)
+            else if(k > Arr[mid])
             {
                 s = mid+1;
             }
@@ -39,7 +44,6 @@ class Solution{
                 e = mid-1;
             }
         }
-        return mid+1;
     }
 };
 
