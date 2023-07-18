64835ae0b044b8f16d85cb4fc15f1241cfb7df76
Vivek yadav <20bec069@nith.ac.in>
2023-07-18 14:07:13 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
@@ -5,35 +5,33 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 	public:
-	 int solve(string& s1,string& s2,int m,int n,vector<vector<int>>& dp)
-    {
-        if(m==0 || n==0)
-        {
-            return 0;
-        }
-       
-        
-        if(dp[m][n] != -1)
-        {
-            return dp[m][n];
-        }
-        
-        if(s1[m-1] == s2[n-1] && (m-1) != (n-1))
-        {
-            return dp[m][n] = 1+solve(s1,s2,m-1,n-1,dp);
-        }
-        else
-        {
-            return dp[m][n] = max(solve(s1,s2,m-1,n,dp),solve(s1,s2,m,n-1,dp));
-        }
-    }
-    
+	    int solve(int i,int j,string& s,string& t,vector<vector<int>>& dp)
+	    {
+	        if(i==0 || j==0)
+	        {
+	            return 0;
+	        }
+	        
+	        if(dp[i][j] != -1)
+	        {
+	            return dp[i][j];
+	        }
+	        
+	        if(i != j && s[i-1] == t[j-1])
+	        {
+	            return dp[i][j] = 1+solve(i-1,j-1,s,t,dp);
+	        }
+	        else
+	        {
+	            return dp[i][j] = max(solve(i-1,j,s,t,dp) , solve(i,j-1,s,t,dp));
+	        }
+	    }
+	    
 		int LongestRepeatingSubsequence(string str){
-		   int n = str.length();
-		   vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-		   return solve(str,str,n,n,dp);
+		    int n = str.length();
+		    vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+		    return solve(n,n-1,str,str,dp);
 		}
-
 };
 
 //{ Driver Code Starts.
