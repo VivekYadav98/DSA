76bf235c7b25e34864142aa01d261a511de1c0d5
Vivek yadav <20bec069@nith.ac.in>
2023-07-21 19:40:02 +0530

    Added solution - LeetHub

--- Search in a matrix - GFG/search-in-a-matrix.cpp
+++ Search in a matrix - GFG/search-in-a-matrix.cpp
@@ -10,21 +10,23 @@ public:
 	int matSearch (vector <vector <int>> &mat, int N, int M, int X)
 	{
 	    int i=0,j=M-1;
+	    
 	    while(i<N && j>=0)
 	    {
 	        if(mat[i][j] == X)
 	        {
 	            return 1;
 	        }
-	        else if(X < mat[i][j])
-	        {
-	            j--;
-	        }
-	        else
+	        else if(X > mat[i][j])
 	        {
 	            i++;
 	        }
+	        else
+	        {
+	            j--;
+	        }
 	    }
+	    
 	    return 0;
 	}
 };
