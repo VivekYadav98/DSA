49d4512314925a407f58c4ba7a88dc716a123f27
Vivek yadav <20bec069@nith.ac.in>
2023-07-23 22:06:13 +0530

    Added solution - LeetHub

--- Triangle Path Sum - GFG/triangle-path-sum.cpp
+++ Triangle Path Sum - GFG/triangle-path-sum.cpp
@@ -5,25 +5,38 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int solve(int i,int j, vector<vector<int>>& triangle,int n,vector<vector<int>>& dp)
-  {
-      if(i==n-1)
-      {
-          return triangle[n-1][j];
-      }
-      
-      if(dp[i][j] != -1)
-      {
-          return dp[i][j];
-      }
-      
-      int down = triangle[i][j] + solve(i+1,j,triangle,n,dp);
-      int diag = triangle[i][j] + solve(i+1,j+1,triangle,n,dp);
-      return dp[i][j] = min(down,diag);
-  }
+    int solve(int i,int j,int n,vector<vector<int>>& triangle,vector<vector<int>>& dp)
+    {
+        if(i == n-1)
+        {
+            return triangle[i][j];
+        }
+        
+        if(i>=n)
+        {
+            return 1e8;
+        }
+        
+        int m = triangle[i].size();
+        if(j>=m)
+        {
+            return 1e8;
+        }
+        
+        if(dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+        
+        int ans1 = triangle[i][j] + solve(i+1,j,n,triangle,dp);
+        int ans2 = triangle[i][j] + solve(i+1,j+1,n,triangle,dp);
+        return dp[i][j] = min(ans1,ans2);
+    }
+    
     int minimizeSum(int n, vector<vector<int>>& triangle) {
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return solve(0,0,triangle,n,dp);
+        int m = triangle[n-1].size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return solve(0,0,n,triangle,dp);
     }
 };
 
